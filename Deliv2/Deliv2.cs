4abfaadfc3a56a10be5a48c15b783f0e1250e5bb
Rob <32168252+Kontz@users.noreply.github.com>
2019-03-18 00:45:49 -0400

    Add files via upload

---
+++ Deliv2/Deliv2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deliv2
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("What are you in the mood to do?");
+           
+                Console.WriteLine("Here are your options:");
+                Console.WriteLine("    1) Action");
+                Console.WriteLine("    2) Chilling");
+                Console.WriteLine("    3) Danger");
+                Console.WriteLine("    4) Good Food");
+                string userInput = Console.ReadLine(); //user enters an integer here.  
+                int act1 = 0;
+                
+                while(!Int32.TryParse(userInput, out act1) || act1 <=0 || act1 >= 5) //makes sure the input is an integer
+                    {
+                    Console.WriteLine("Selection not valid.  Please choose again and pick an option from above.");
+                    userInput = Console.ReadLine();
+                }
+
+            string userAct = Convert.ToString(act1); 
+
+                switch (act1)
+                {
+                    case 1:
+                    userAct = "Since you want action, you should go Stock Car Racing";
+                       break;
+                    case 2:
+                    userAct = "If you want to chill, you should go Hiking";
+                        break;
+                    case 3:
+                    userAct = "You're wanting danger, then you should go Skydiving";
+                        break;
+                    default:
+                    userAct = "If you want good food, head to Taco Bell";
+                        break;
+                }
+
+            Console.WriteLine("How many people are you bringing with you?");
+            string peepsInput = Console.ReadLine();
+            int totPeople = 0;
+
+            while (!Int32.TryParse(peepsInput, out totPeople) || totPeople <0)
+            {
+                Console.WriteLine("Invalid option.  Please enter the number of people you are bringing with you.");
+                peepsInput = Console.ReadLine();
+            }
+
+            string travMode = Convert.ToString(totPeople);
+
+            if (totPeople == 0)
+            {
+                travMode = "sneakers";
+            }
+            else if (totPeople >= 1 && totPeople <= 4)
+            {
+                travMode = "a sedan";
+            }
+            else if (totPeople >= 5 && totPeople <= 10)
+            {
+                travMode = "a Volkswagen bus";
+            }
+            else
+            {
+                travMode = "an airplane";
+            }
+
+            Console.WriteLine(userAct + " and you should use " + travMode + ".");
+            Console.WriteLine( "Have fun and good bye!");
+        }
+
+
+
+    }
+}
---
+++ Deliverable 1/Deliverable1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deliverable_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uSD;
+            string jD;
+            string sD;
+            string tD;
+
+            Console.WriteLine("Please enter three numbers:");
+            var num1 = float.Parse(Console.ReadLine());
+            var num2 = float.Parse(Console.ReadLine());
+            var num3 = float.Parse(Console.ReadLine());
+
+            float totNum = (num1 + num2 + num3);
+            Console.WriteLine("The total number is " + totNum);
+
+            float avNum = (totNum) / 3;
+            Console.WriteLine("The average number is " + avNum);
+
+            if (num1 > num2 && num1 > num2)
+            {
+                Console.WriteLine(num1 + " is the highest number");
+            }
+                        else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine(num2 + " is the highest number");
+            }
+            else
+            {
+                Console.WriteLine(num3 + " is the highest number");
+            }
+
+            if (num1 < num2 && num1 < num2)
+
+            {
+                Console.WriteLine(num1 + " is the lowest number");
+            }
+            else if (num2 < num1 && num2 < num3)
+            {
+                Console.WriteLine(num2 + " is the lowest number");
+            }
+            else
+            {
+                Console.WriteLine(num3 + " is the lowest number");
+            }
+
+            uSD = totNum.ToString("C2");
+            jD = totNum.ToString("C0", CultureInfo.CreateSpecificCulture("ja-JP"));
+            sD = totNum.ToString("C2", CultureInfo.CreateSpecificCulture("sv-SEK"));
+            tD = totNum.ToString("C2", CultureInfo.CreateSpecificCulture("th-THB"));
+
+            Console.WriteLine("US Dollar: " + uSD);
+            Console.WriteLine("Japanese Yen: " + jD);
+            Console.WriteLine("Swidish Krona: " + sD);
+            Console.WriteLine("Thailand Baht: " + tD);
+
+        }
+    } 
+}
